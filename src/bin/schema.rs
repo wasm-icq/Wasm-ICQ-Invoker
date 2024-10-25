347035d644bbdaed7ac14043f322f33d036f8ee2
Raumo0 <dharapko@gmail.com>
2024-10-25 17:32:34 +0400

    CosmWasm contract template has been created

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use wasm_icq_invoker::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
